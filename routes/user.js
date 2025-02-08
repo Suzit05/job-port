b8ba1c6e48737a8ec126b1f57e1e6a9dc650efff
Suzit05 <103447157+Suzit05@users.noreply.github.com>
2025-02-08 22:48:54 +0530

    user routes updated

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const userModel = require("../models/user.model");
+const applicatonModel = require("../models/application.model");
+const applicationModel = require("../models/application.model");
 const router = express.Router()
 
 
@@ -49,7 +51,55 @@ router.put("/", async (req, res, next) => {
     }
 });
 
-//start from here
+//api/user/jobs?createdJobs=true&savedJobs=false&appliedJobs=true
+
+router.get("/jobs", async (req, res, next) => {
+    try {
+        const { createdJobs, savedJobs, appliedJobs } = req.query;
+        const jobs = await userModel.findById(req.user.id)
+        if (!jobs) {
+            return res.status(404).json({ message: "User not found" })
+        }
+
+        const query = {}
+        if (createdJobs) {
+            query.createdJobs = true;
+        }
+        if (savedJobs) {
+            query.savedJobs = true;
+        }
+        if (appliedJobs) {
+            query.appliedJobs = true;
+        }
+
+        else {
+            const jobs = await userModel.findById(req.user.id).select({ ...query })
+            return res.status(200).json(jobs)
+        }
+    }
+    catch (err) {
+        next(err)
+    }
+})
+
+router.get("/status", async (req, res, next) => {
+    try {
+        const user = await userModel.findById(id)
+        if (!user) {
+            return res.status(404).json({ message: "User not found" })
+        }
+        const applications = await applicatonModel.find({ user: id });
+        const accceptedApplications = applications.filter(app => app.status === "accepted"); //accepted app hi dikhega
+        return res.json({ accceptedApplications }).status(200)
+    }
+    catch (err) {
+        next(err)
+    }
+})
+
+//create filters for pending and rejected
+//create apis for counting application (individually ) and cumulatively
+
 
 
 
